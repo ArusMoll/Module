4f8514bb45050efa4449eac0a156d5c8ffa3202c
Arsen <arusfall@gmail.com>
2025-03-16 14:52:03 +0200

    update

--- Module/Form1.Designer.cs
+++ Module/Form1.Designer.cs
@@ -28,28 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            flowLayoutPanel1 = new FlowLayoutPanel();
             textBox1 = new TextBox();
             button1 = new Button();
+            lstToDo = new ListBox();
+            lstInProgress = new ListBox();
+            lstDone = new ListBox();
             SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            flowLayoutPanel1.Location = new Point(12, 12);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(776, 248);
-            flowLayoutPanel1.TabIndex = 0;
             // 
             // textBox1
             // 
             textBox1.Location = new Point(12, 266);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(776, 23);
+            textBox1.Size = new Size(672, 23);
             textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(699, 306);
+            button1.Location = new Point(609, 305);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 2;
@@ -57,14 +52,44 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // lstToDo
+            // 
+            lstToDo.FormattingEnabled = true;
+            lstToDo.ItemHeight = 15;
+            lstToDo.Location = new Point(12, 12);
+            lstToDo.Name = "lstToDo";
+            lstToDo.Size = new Size(220, 244);
+            lstToDo.TabIndex = 3;
+            // 
+            // lstInProgress
+            // 
+            lstInProgress.FormattingEnabled = true;
+            lstInProgress.ItemHeight = 15;
+            lstInProgress.Location = new Point(238, 12);
+            lstInProgress.Name = "lstInProgress";
+            lstInProgress.Size = new Size(220, 244);
+            lstInProgress.TabIndex = 4;
+            lstInProgress.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // lstDone
+            // 
+            lstDone.FormattingEnabled = true;
+            lstDone.ItemHeight = 15;
+            lstDone.Location = new Point(464, 12);
+            lstDone.Name = "lstDone";
+            lstDone.Size = new Size(220, 244);
+            lstDone.TabIndex = 5;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(687, 450);
+            Controls.Add(lstDone);
+            Controls.Add(lstInProgress);
+            Controls.Add(lstToDo);
             Controls.Add(button1);
             Controls.Add(textBox1);
-            Controls.Add(flowLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -73,9 +98,10 @@
         }
 
         #endregion
-
-        private FlowLayoutPanel flowLayoutPanel1;
         private TextBox textBox1;
         private Button button1;
+        private ListBox lstToDo;
+        private ListBox lstInProgress;
+        private ListBox lstDone;
     }
 }
